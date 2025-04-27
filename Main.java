57fa8b63c3447eb64630a0b76c52f7c12f1b4085
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-26 21:23:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
         public static void main(String[] args) {
             System.out.println("Collective Guide");
+            System.out.println("To make a business/partnership/company/corporation");
+            System.out.println("Make a summary of the collective");
+            System.out.println("Designate board of directors roles (R&D, Revenue, Management/Administration, Purchasing, Production)");
+            System.out.println("Make a google firebase host");
         }
     }
